e19c6f1ccb5692d1083cf073e40a529b90219075
Nur Akmal <mdnurakmal@gmail.com>
2021-12-18 18:02:18 +0800

    update

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.53"
+    }
+  }
+
+    backend "gcs" {
+  }
+
+}
+
+
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+}
+
---
+++ variables.tf
@@ -0,0 +1,6 @@
+
+# define GCP project name
+variable "project_id" {
+  type        = string
+  description = "GCP project name"
+}
