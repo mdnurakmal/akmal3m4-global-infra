5c4f7af0e1836254e2c7d9e629adf2975084a176
Nur Akmal <mdnurakmal@gmail.com>
2021-12-18 20:01:04 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -90,12 +90,14 @@ resource "google_compute_global_forwarding_rule" "http-forwarding-rule" {
   name       = "http-forwarding-rule"
   target     = google_compute_target_http_proxy.default.id
   port_range = "80"
+  ip_address = google_compute_global_address.static-ip.id
 }
 
 resource "google_compute_global_forwarding_rule" "https-forwarding-rule" {
   name       = "https-forwarding-rule"
   target     = google_compute_target_https_proxy.default.id
   port_range = "443"
+  ip_address = google_compute_global_address.static-ip.id
 }
 
 resource "google_compute_target_http_proxy" "default" {
