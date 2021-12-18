be5814ce3b2229a88c9c6d33729c114de0271f87
Nur Akmal <mdnurakmal@gmail.com>
2021-12-18 18:10:11 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,3 +17,38 @@ provider "google" {
   region      = var.region
 }
 
+
+
+resource "google_compute_backend_service" "game-server-backend-service" {
+  provider = google-beta
+  project = var.project_id
+  name                            = "game-server-backend-service-${var.multiregion}"
+  enable_cdn                      = true
+  connection_draining_timeout_sec = 10
+
+  backend {
+  group = "projects/${var.project_id}/regions/asia-southeast1/networkEndpointGroups/game-server-asia-neg"
+  }
+
+    backend {
+  group = "projects/${var.project_id}/regions/us-central1/networkEndpointGroups/game-server-us-neg"
+  }
+
+
+}
+
+resource "google_compute_backend_service" "game-client-backend-service" {
+  provider = google-beta
+  project = var.project_id
+  name                            = "game-client-backend-service-${var.multiregion}"
+  enable_cdn                      = true
+  connection_draining_timeout_sec = 10
+
+  backend {
+    group = "projects/${var.project_id}/regions/asia-southeast1/networkEndpointGroups/game-client-asia-neg"
+  }
+
+    backend {
+    group = "projects/${var.project_id}/regions/us-central1/networkEndpointGroups/game-client-us-neg"
+  }
+}
